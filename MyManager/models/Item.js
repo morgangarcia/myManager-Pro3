aa2709e40fae5b54990d1e167b2cfc7c30f0c993
Morgan Garcia <morgangarcia1977@gmail.com>
2019-04-06 17:03:10 -0400

    Backend1

---
+++ MyManager/models/Item.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+const ItemSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Date,
+        default: Date.now
+    }
+});
+module.exports = Item = mongoose.model('model', ItemSchema);
